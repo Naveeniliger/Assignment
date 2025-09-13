13df285b265b81b57a740ccedf19c393775bbf18
Naveen Iliger <naveeniligerm@gmail.com>
2025-09-13 20:22:41 +0530

    add quiz slice and store configuration

---
+++ src/store/quizSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  questions: [],
+  currentQuestionIndex: 0,
+  answers: {},
+  isSubmitted: false,
+};
+
+export const quizSlice = createSlice({
+  name: 'quiz',
+  initialState,
+  reducers: {
+    setQuestions: (state, action) => {
+      state.questions = action.payload;
+    },
+    selectAnswer: (state, action) => {
+      const { questionId, answerIndex } = action.payload;
+      state.answers[questionId] = answerIndex;
+    },
+    nextQuestion: (state) => {
+      if (state.currentQuestionIndex < state.questions.length - 1) {
+        state.currentQuestionIndex += 1;
+      }
+    },
+    prevQuestion: (state) => {
+      if (state.currentQuestionIndex > 0) {
+        state.currentQuestionIndex -= 1;
+      }
+    },
+    submitQuiz: (state) => {
+      state.isSubmitted = true;
+    },
+  },
+});
+
+export const { setQuestions, selectAnswer, nextQuestion, prevQuestion, submitQuiz } = quizSlice.actions;
+export const quizReducer = quizSlice.reducer;
